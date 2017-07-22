0399965f55b755ff99013920e11fc5668c1b17f3
Yusto <onesmusmakori@gmail.com>
2017-07-22 18:56:43 +0300

    final

--- app/src/main/java/com/example/swee/tabibu/Home.java
+++ app/src/main/java/com/example/swee/tabibu/Home.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -105,6 +106,7 @@ public class Home extends AppCompatActivity
 
 
         } else if (id == R.id.nav_share) {
+
 
         } else if (id == R.id.nav_send) {
 
